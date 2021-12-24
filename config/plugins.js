a369b46cd0d4b2930a373a146dce7c08a5da09ef
Kawai cheung <kawaicheungdev@gmail.com>
2021-12-24 13:56:57 +0100

    Test again

--- config/plugins.js
+++ config/plugins.js
@@ -14,6 +14,7 @@ module.exports = ({ env }) => ({
       endpoint: "/graphql",
       shadowCRUD: true,
       playgroundAlways: true,
+      introspection: true,
       depthLimit: 7,
       amountLimit: 100,
       apolloServer: {
